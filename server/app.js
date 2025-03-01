9d249e15b0caaea93f490312bf9c852ce5fa0959
vicmarchesi <victor.marchesi91@gmail.com>
2025-03-01 13:59:38 +0000

    REST API Day 3
    Co-authored-by: Paulo Prado <paulroar@users.noreply.github.com>

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-const PORT = 5005;
 const cors = require("cors");
+
 
 const Cohort = require('./models/Cohort.model');
 const Students = require('./models/Students.model');
@@ -19,8 +20,7 @@ const students = require("./students.json")
 
 const mongoose = require("mongoose");
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+mongoose.connect(process.env.MONGODB_URI)
   .then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
   .catch(err => console.error("Error connecting to MongoDB", err));
 
@@ -49,40 +49,135 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
-app.get("/api/cohorts", (req, res) => {
-  res.json(cohorts);
-});
+// app.get("/api/cohorts", (req, res) => {
+//   res.json(cohorts);
+// });
 
-app.get("/api/students", (req, res) => {
-  res.json(cohorts);
-});
+// app.get("/api/students", (req, res) => {
+//   res.json(cohorts);
+// });
 
 app.get("/cohorts", (req, res) => {
   Cohort.find({})
-    .then((cohort) => {
+    .then((cohorts) => {
       console.log("Retrieved cohort ->", cohorts);
       res.json(cohorts);
     })
     .catch((error) => {
-      console.error("Error while retrieving cohorts ->", error);
-      res.status(500).json({ error: "Failed to retrieve cohorts" });
+      console.error("Error while getting cohorts ->", error);
+      res.status(500).json({ error: "Failed to get cohorts" });
     });
 });
 
-app.get("/students", (req, res) => {
-  Students.find({})
-    .then((students) => {
-      console.log("Retrieved students ->", students);
-      res.json(Students);
-    })
-    .catch((error) => {
+app.get("/cohorts/:cohortId", async (req, res) => {
+  try {
+    const cohort = await Cohort.findById(req.params.cohortId); 
+    if (!cohort) {
+      return res.status(404).json({ error: 'Cohort not found' });
+    }
+    res.json(cohort);
+  } catch (error) {
+    console.error('Error getting cohort:', error); 
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+      return res.status(400).json({ error: 'Invalid cohort ID format' });
+    }
+    res.status(500).json({ error: 'failed to get cohort' }); 
+  }
+});
+
+app.post('/cohorts', async (req, res) => {
+  try {
+    const newCohort = await Cohort.create(req.body);
+    res.status(201).json(newCohort);
+  } catch (error) {
+    console.error('Error creating cohort:', error);
+    res.status(500).json({ error: 'Failed to create cohort' });
+  }
+});
+
+app.put('/cohorts/:cohortId', async (req, res) => {
+  try {
+    const updatedCohort = await Cohort.findByIdAndUpdate(req.params.cohortId, req.body, { new: true })
+    if (!updatedCohort) {
+      return res.status(404).json({ error: 'Cohort not found' });
+    }
+    res.json(updatedCohort);
+  } catch (error) {
+    console.error('Error updating cohort:', error);
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+      return res.status(400).json({ error: 'Invalid cohort ID format' });
+    }
+    res.status(500).json({ error: 'Failed to update cohort' });
+  }
+});
+
+app.delete('/cohorts/:cohortId', async (req, res) => {
+  try {
+    const deletedCohort = await Cohort.findByIdAndDelete(req.params.cohortId);
+    if (!deletedCohort) {
+      return res.status(404).json({ error: 'Cohort not found' });
+    }
+    res.status(204).send(); 
+  } catch (error) {
+    console.error('Error deleting cohort:', error);
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+      return res.status(400).json({ error: 'Invalid cohort ID format' });
+    }
+    res.status(500).json({ error: 'Failed to delete cohort' });
+  }
+});
+
+app.get("/students", async (req, res) => {
+  try {
+    const students = await Students.find().populate('cohort')
+    res.json(students);
+  } catch (error) {
       console.error("Error while retrieving students ->", error);
       res.status(500).json({ error: "Failed to retrieve students" });
-    });
+  }
+});
+
+app.get("/students/cohort/:cohortId", async (req, res) => {
+  try {
+    const students = await Students.find({ cohort: req.params.cohortId }).populate('cohort');
+    res.json(students);
+  } catch (error) {
+    res.status(500).json({ error: "Error retrieving students for this cohort." });
+  }
+});
+
+app.get("/students/:studentId" , async (req, res) => {
+  try{
+    const student = await Students.findById(req.params.studentId).populate('cohort');
+    if (!student) return res.status(404).json({ message: "Student not found." });
+    res.json(student);
+  } catch (error) {
+    res.status(500).json({ error: "Error retrieving student." });
+  }
+});
+
+app.post("/students", async (req, res) => {
+    try {  
+      const newStudent = await Students.create(req.body);
+      res.status(201).json(newStudent);
+    } catch (error) {
+      console.error("Error creating student:", error);
+      res.status(500).json({ error: "Error creating student." });
+    }
+  });
+
+app.delete("/students/:studentId", async (req, res) => {
+  try {
+    const student = await Students.findByIdAndDelete(req.params.studentId);
+            
+    res.json({ message: "Student deleted successfully." });
+  } catch (error) {
+    res.status(500).json({ error: "Error deleting student." });
+  }
 });
 
 
 // START SERVER
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port ${process.env.PORT}`);
 });
